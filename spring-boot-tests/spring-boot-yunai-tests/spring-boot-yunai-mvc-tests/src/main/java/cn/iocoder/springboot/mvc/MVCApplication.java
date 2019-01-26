74690873857801615f1bf404c229a29986b96238
YunaiV <>
2019-01-26 13:12:23 +0800

    增加 spring boot mvc 的调试 demo

---
+++ spring-boot-tests/spring-boot-yunai-tests/spring-boot-yunai-mvc-tests/src/main/java/cn/iocoder/springboot/mvc/MVCApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MVCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MVCApplication.class, args);
+    }
+
+}
---
+++ spring-boot-tests/spring-boot-yunai-tests/spring-boot-yunai-mvc-tests/src/main/java/cn/iocoder/springboot/mvc/controller/DemoController.java
@@ -0,0 +1,17 @@
+package cn.iocoder.springboot.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "world";
+    }
+
+}
